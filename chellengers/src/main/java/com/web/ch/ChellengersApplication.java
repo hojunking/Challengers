7449a42d2c69b58697c81d10055e989d955dd874
hojun <84840627+hojunking@users.noreply.github.com>
2022-04-27 12:38:55 +0900

    initial

---
+++ chellengers/src/main/java/com/web/ch/ChellengersApplication.java
@@ -0,0 +1,13 @@
+package com.web.ch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChellengersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChellengersApplication.class, args);
+	}
+
+}
---
+++ chellengers/src/test/java/com/web/ch/chellengers/ChellengersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.ch.chellengers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChellengersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
