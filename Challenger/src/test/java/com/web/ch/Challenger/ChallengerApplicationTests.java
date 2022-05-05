26e8a59d1dc7a2b1757bcaf65a15fb07d02baa29
hojun <84840627+hojunking@users.noreply.github.com>
2022-05-05 19:29:31 +0900

    Restart

---
+++ Challenger/src/test/java/com/web/ch/Challenger/ChallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.ch.Challenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
