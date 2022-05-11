1c519c3a44c3b861e5c5160916a9369255525699
whipbaek <jibaek0513@gmail.com>
2022-05-11 19:58:17 +0900

    whip commit

--- challengers/src/main/java/com/web/ch/challengers/test/domain/EmployeeVO.java
+++ challengers/src/main/java/com/web/ch/challengers/test/domain/EmployeeVO.java
@@ -2,6 +2,11 @@ package com.web.ch.challengers.test.domain;
 
 import lombok.Data;
 
+/**
+ * lombok의 @Data 어노테이션은
+ * @Getter,@Setter,@RequiredArgsConstructor,@ToString,@EqualsAndHashCode 를
+ * 한번에 설정해주는 어노테이션이다.
+ */
 @Data
 public class EmployeeVO {
     private int employeeId;
